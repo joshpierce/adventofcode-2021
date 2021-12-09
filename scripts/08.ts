d4292619f02080d559b4b603cf1abaf17e601f27
Joshua Pierce <joshua@joshuapierce.com>
2021-12-09 04:32:52 -0600

    day 9 solve

--- scripts/08.ts
+++ scripts/08.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import { networkInterfaces } from 'os';
 import path from 'path';
 
 const run08 = () => {
@@ -32,9 +33,128 @@ const run08 = () => {
 
     console.log(uniqueOutputs);
 
+    //     0: 6    1: 2    2: 5   3: 5    4: 4
+    //     aaaa    ....    aaaa    aaaa    ....
+    //    b    c  .    c  .    c  .    c  b    c
+    //    b    c  .    c  .    c  .    c  b    c
+    //     ....    ....    dddd    dddd    dddd
+    //    e    f  .    f  e    .  .    f  .    f
+    //    e    f  .    f  e    .  .    f  .    f
+    //     gggg    ....    gggg    gggg    ....
+
+    //     5: 5    6: 6    7: 3    8: 7    9: 6
+    //     aaaa    aaaa    aaaa    aaaa    aaaa
+    //    b    .  b    .  .    c  b    c  b    c
+    //    b    .  b    .  .    c  b    c  b    c
+    //     dddd    dddd    ....    dddd    dddd
+    //    .    f  e    f  .    f  e    f  .    f
+    //    .    f  e    f  .    f  e    f  .    f
+    //     gggg    gggg    ....    gggg    gggg
+
+    const digits = [
+        { bits: '1110111', digit: '0' },
+        { bits: '0000011', digit: '1' },
+        { bits: '0111110', digit: '2' },
+        { bits: '0011111', digit: '3' },
+        { bits: '1001011', digit: '4' },
+        { bits: '1011101', digit: '5' },
+        { bits: '1111101', digit: '6' },
+        { bits: '0010011', digit: '7' },
+        { bits: '1111111', digit: '8' },
+        { bits: '1011111', digit: '9' },
+    ];
+
+    let sumOfOutputs = 0;
+
     inputs.forEach((input, vx) => {
-        input.outputs.forEach((output, ox) => {});
+        let grid: string[][] = new Array(7).fill(new Array());
+        //Identify the one
+        let one = input.entries.find((e) => e.length === 2);
+        one?.split('').forEach((seg) => {
+            grid[5] = [...grid[5], seg];
+            grid[6] = [...grid[6], seg];
+        });
+        //Identify the seven
+        let seven = input.entries.find((e) => e.length === 3);
+        seven?.split('').forEach((seg) => {
+            if (grid[5].indexOf(seg) == -1 && grid[6].indexOf(seg) == -1) {
+                grid[2] = [...grid[2], seg];
+            }
+        });
+        //Identify the four
+        let four = input.entries.find((e) => e.length === 4);
+        four?.split('').forEach((seg) => {
+            if (grid[5].indexOf(seg) == -1 && grid[6].indexOf(seg) == -1) {
+                grid[0] = [...grid[0], seg];
+                grid[3] = [...grid[3], seg];
+            }
+        });
+        //Identify the zero
+        let zero = input.entries.find((e) => {
+            //Must be 6 in length, and not contain both elements in index 3 of the grid (middle middle)
+            return (
+                e.length === 6 &&
+                e.split('').filter((seg) => grid[3].indexOf(seg) != -1)
+                    .length == 1
+            );
+        });
+        grid[0] = [
+            ...grid[0].filter((seg) => zero?.split('').indexOf(seg) !== -1),
+        ];
+        grid[3] = [...grid[3].filter((seg) => seg !== grid[0][0])];
+        //Identify the six
+        let six = input.entries.find((e) => {
+            return (
+                e.length === 6 &&
+                e != zero &&
+                e.split('').filter((seg) => grid[5].indexOf(seg) != -1)
+                    .length === 1
+            );
+        });
+        grid[6] = [
+            ...grid[6].filter((seg) => six?.split('').indexOf(seg) !== -1),
+        ];
+        grid[5] = [...grid[5].filter((seg) => seg !== grid[6][0])];
+        //Identify the nine
+        let nine = input.entries.find((e) => {
+            return e.length === 6 && [zero, six].indexOf(e) === -1;
+        });
+        let bottomLeft = six?.split('').find((seg) => {
+            return nine?.indexOf(seg) === -1 && seg != grid[5][0];
+        });
+        if (bottomLeft) {
+            grid[1] = [bottomLeft];
+        }
+        let bottomMiddle = 'abcdefg'.split('').find((seg) => {
+            return grid.map((g) => g[0]).indexOf(seg) === -1;
+        });
+        if (bottomMiddle) {
+            grid[4] = [bottomMiddle];
+        }
+
+        let consolidatedGrid = grid.map((g) => g[0]);
+
+        let outDigits: string[] = [];
+        input.outputs.forEach((out, ox) => {
+            outDigits = [
+                ...outDigits,
+                digits.find((d) => {
+                    let bitCheck = '0000000'.split('');
+                    out.split('').forEach((o, x) => {
+                        bitCheck[consolidatedGrid.indexOf(o)] = '1';
+                    });
+                    return d.bits === bitCheck.join('');
+                })?.digit as string,
+            ];
+        });
+
+        console.log(`The output digits are ${outDigits.join('')}`);
+
+        sumOfOutputs += parseInt(outDigits.join(''));
     });
+
+    console.log();
+    console.log(`The sum of all the outputs is ${sumOfOutputs}`);
 };
 
 export { run08 };
--- scripts/09.ts
+++ scripts/09.ts
@@ -12,7 +12,132 @@ const run09 = () => {
         .toString()
         .split(/\r?\n/);
 
-    //TODO: Write some code here
+    let grid = input.map((x) => {
+        return x.split('').map(Number);
+    });
+
+    let lowPoints = grid.reduce((acc, val, idx) => {
+        return [
+            ...acc,
+            ...val.reduce((acc2, val2, idx2) => {
+                if (
+                    (idx - 1 < 0 || val2 < grid[idx - 1][idx2]) && //up
+                    (idx + 1 == grid.length || val2 < grid[idx + 1][idx2]) && //down
+                    (idx2 - 1 < 0 || val2 < grid[idx][idx2 - 1]) && //left
+                    (idx2 + 1 == val.length || val2 < grid[idx][idx2 + 1]) //right
+                ) {
+                    return [...acc2, [idx, idx2]];
+                } else {
+                    return acc2;
+                }
+            }, [] as number[][]),
+        ];
+    }, [] as number[][]);
+
+    let sumOfRiskLevel = lowPoints.reduce((acc, val) => {
+        return acc + grid[val[0]][val[1]] + 1;
+    }, 0);
+
+    console.log(`The sum of risk level of all low points is ${sumOfRiskLevel}`);
+
+    const getBasinSize = (
+        lowPoint: number[],
+        basinPoints: { point: number[]; checked: boolean }[]
+    ): number => {
+        if (!basinPoints || basinPoints.length === 0) {
+            basinPoints = [{ point: lowPoint, checked: false }];
+        }
+        let checkPoints = [...basinPoints.filter((p) => !p.checked)];
+
+        checkPoints.forEach((p, px) => {
+            // Check Up
+            if (
+                p.point[0] - 1 >= 0 &&
+                grid[p.point[0] - 1][p.point[1]] < 9 &&
+                basinPoints.filter(
+                    (bp) =>
+                        bp.point[0] == p.point[0] - 1 &&
+                        bp.point[1] == p.point[1]
+                ).length == 0
+            ) {
+                basinPoints = [
+                    ...basinPoints,
+                    { point: [p.point[0] - 1, p.point[1]], checked: false },
+                ];
+            }
+            // Check Down
+            if (
+                p.point[0] + 1 < grid.length &&
+                grid[p.point[0] + 1][p.point[1]] < 9 &&
+                basinPoints.filter(
+                    (bp) =>
+                        bp.point[0] == p.point[0] + 1 &&
+                        bp.point[1] == p.point[1]
+                ).length == 0
+            ) {
+                basinPoints = [
+                    ...basinPoints,
+                    { point: [p.point[0] + 1, p.point[1]], checked: false },
+                ];
+            }
+            // Check Left
+            if (
+                p.point[1] - 1 >= 0 &&
+                grid[p.point[0]][p.point[1] - 1] < 9 &&
+                basinPoints.filter(
+                    (bp) =>
+                        bp.point[0] == p.point[0] &&
+                        bp.point[1] == p.point[1] - 1
+                ).length == 0
+            ) {
+                basinPoints = [
+                    ...basinPoints,
+                    { point: [p.point[0], p.point[1] - 1], checked: false },
+                ];
+            }
+            // Check Down
+            if (
+                p.point[1] + 1 < grid[0].length &&
+                grid[p.point[0]][p.point[1] + 1] < 9 &&
+                basinPoints.filter(
+                    (bp) =>
+                        bp.point[0] == p.point[0] &&
+                        bp.point[1] == p.point[1] + 1
+                ).length == 0
+            ) {
+                basinPoints = [
+                    ...basinPoints,
+                    { point: [p.point[0], p.point[1] + 1], checked: false },
+                ];
+            }
+            let markPoint = basinPoints.find(
+                (bp) => bp.point[0] == p.point[0] && bp.point[1] == p.point[1]
+            );
+            if (markPoint) {
+                markPoint.checked = true;
+            }
+        });
+
+        if (basinPoints.filter((bp) => !bp.checked).length === 0) {
+            return basinPoints.length;
+        } else {
+            return getBasinSize(lowPoint, basinPoints);
+        }
+    };
+
+    let basinSizes = lowPoints.map((lp) => {
+        return getBasinSize(lp, []);
+    });
+
+    basinSizes.sort((a, b) => b - a);
+
+    console.log(
+        `The three largest basin sizes are ${basinSizes[0]}, ${
+            basinSizes[1]
+        }, and ${basinSizes[2]} for a multiplied answer of ${
+            basinSizes[0] * basinSizes[1] * basinSizes[2]
+        }`
+    );
 };
 
 export { run09 };
