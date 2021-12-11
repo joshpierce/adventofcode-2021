fa58e4887d14fa7b1ec624e3f39a46f525cb7887
Joshua Pierce <joshua@joshuapierce.com>
2021-12-10 22:45:30 -0600

    day 10 part 1 solve

--- scripts/10.ts
+++ scripts/10.ts
@@ -29,54 +29,45 @@ const run10 = () => {
         line: string,
         openToClose: string[]
     ) => {
-        console.log(`${line.length} characters left to check for Errors.`);
-        let currentChar = line.split('')[0];
-        console.log(`Check character ${currentChar}`);
+        // Get the first character from a string
+        let currentChar = line.charAt(0);
+        // Figure out which pair the character belongs to
         let currentPair = charPairs.find(
             (pair) => pair.close === currentChar || pair.open === currentChar
         );
-        console.log('found current pair', currentPair);
+        // Determine if the character is an open or close character
         let isClosing = currentChar === currentPair?.close;
-        // We're either breaking, or returning openToClose with one less element
-        if (isClosing) {
-            console.log(`Found a Closing Character ${currentChar}`);
-            if (openToClose.slice(-1)[0] == currentPair?.open) {
-                console.log(
-                    'Correct Closing Character for currently open Chunk'
-                );
-                line = line.split('').slice(1).join('');
-                openToClose = openToClose.slice(0, openToClose.length - 1);
-                return checkLine(line, openToClose);
-            } else {
-                console.log(
-                    'Wrong Closing Character for current Chunk, Returning.'
-                );
-                if (currentPair) {
-                    return currentPair.points;
-                } else {
-                    return -1;
-                }
-            }
-        } else if (openToClose.length == 1) {
-            line = line.split('').slice(1).join('');
-            openToClose = [...openToClose, currentChar];
-            console.log(`New backlog contains ${openToClose}`);
-            return checkLine(line, openToClose);
+
+        // If the character is an open character, add it to the openToClose array
+        if (currentPair?.open === currentChar) {
+            openToClose.push(currentChar);
         }
 
-        if (line.length === 0) {
-            console.log('zero');
+        // If the character is a close character, and matches it's corresponding open character, remove it's corresponding open character from the openToClose array
+        if (currentPair?.close === currentChar) {
+            let openChar = openToClose.pop();
+            if (openChar !== currentPair?.open) {
+                return currentPair.points;
+            }
+        }
+
+        // If there are still characters remaining in the line, recursively call the function
+        if (line.length > 1) {
+            return checkLine(line.substr(1), openToClose);
+        } else {
             return 0;
         }
-
-        return -1;
     };
 
-    let errorSum = input.map((line) => {
-        checkLine(line, []);
-    });
+    let errorSum = input
+        .map((line) => {
+            return checkLine(line, []);
+        })
+        .reduce((acc, val) => {
+            return acc + val;
+        });
 
-    console.log(errorSum);
+    console.log(`The total error syntax score is ${errorSum}`);
 };
 
 export { run10 };
